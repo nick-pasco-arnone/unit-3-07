4e5493cf6c8876c1fa4ce58e14b8209bcd362f57
Nick <nicholas.pasco-arnone@ocsbstudent.ca>
2018-10-25 16:30:52 -0400

    Initial commit

---
+++ 3-07.playground/Contents.swift
@@ -0,0 +1,83 @@
+// Created on October 25
+// Created by Nicholas Pasco-Arnone
+// Created for ICS3U
+// This program calculates a factorial number 
+
+import PlaygroundSupport
+
+
+import UIKit
+
+class ViewController : UIViewController {
+    // this is the main view controller, that will show the UIKit elements
+    
+    // properties
+    
+    let instructionLabel = UILabel()
+    var numberTextField = UITextField()
+    let checkButton = UIButton()
+    var answerLabel = UILabel()
+    
+    override func viewDidLoad() {
+        // UI
+        super.viewDidLoad()
+        
+        let view = UIView()
+        view.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        self.view = view
+        
+        instructionLabel.text = "Enter a number"
+        view.addSubview(instructionLabel)
+        instructionLabel.translatesAutoresizingMaskIntoConstraints = false
+        instructionLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 20).isActive = true
+        instructionLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        
+        numberTextField.borderStyle = UITextBorderStyle.roundedRect
+        numberTextField.placeholder = "Enter number"
+        view.addSubview(numberTextField)
+        numberTextField.translatesAutoresizingMaskIntoConstraints = false
+        numberTextField.topAnchor.constraint(equalTo: instructionLabel.bottomAnchor, constant: 20).isActive = true
+        numberTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        
+        
+        checkButton.setTitle("Calculate", for: .normal)
+        checkButton.setTitleColor(.blue, for: .normal)
+        checkButton.titleLabel?.textAlignment = .center
+        checkButton.addTarget(self, action: #selector(calculate), for: .touchUpInside)
+        view.addSubview(checkButton)
+        checkButton.translatesAutoresizingMaskIntoConstraints = false
+        checkButton.topAnchor.constraint(equalTo:numberTextField.bottomAnchor, constant: 20).isActive = true
+        checkButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        
+        answerLabel.text = nil
+        view.addSubview(answerLabel)
+        answerLabel.translatesAutoresizingMaskIntoConstraints = false
+        answerLabel.topAnchor.constraint(equalTo: checkButton.bottomAnchor, constant: 20).isActive = true
+        answerLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+    }
+    
+    
+    
+    
+    @objc func calculate() {
+        
+        var number : Int = Int(numberTextField.text!)!
+        var answer = 1
+        
+        for counter in 1 ... number {
+            answer = answer * counter
+        }
+        answerLabel.text = "the answer is : \(answer)"
+        
+        }
+     
+         
+    }
+    
+    var prefersStatusBarHidden: Bool {
+        return true
+    }
+
+
+// this will be commented out when code moved to Xcode
+PlaygroundPage.current.liveView = ViewController()
